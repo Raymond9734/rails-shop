9a6f321b69e813690cc164e97fef5bc7a021159c
Raymond9734 <rymadara97@gmail.com>
2025-01-23 13:16:28 +0300

    feat(cart): Enhance cart management by merging guest cart items into user cart
    - Implemented logic to merge items from a guest cart into the user's cart upon sign-in.
    - Updated the set_cart method to handle cart creation and session management more efficiently.
    - Added a new method, merge_guest_cart_if_exists, to facilitate the merging process and ensure no items are lost.
    - Improved handling of cart creation to ensure a cart is always available for the user.

--- app/models/concerns/current_cart.rb
+++ app/models/concerns/current_cart.rb
@@ -10,16 +10,35 @@ module CurrentCart
     def set_cart
       if user_signed_in?
         @cart = current_user.cart || current_user.create_cart
-        session[:cart_id] = @cart.id
-      elsif session[:cart_id]
+        merge_guest_cart_if_exists
+      else
         @cart = Cart.find_by(id: session[:cart_id])
-        if @cart.nil?
-          @cart = Cart.create
-          session[:cart_id] = @cart.id
-        end
+        @cart ||= Cart.create
+        session[:cart_id] = @cart.id
       end
-    rescue ActiveRecord::RecordNotFound
-      @cart = Cart.create
-      session[:cart_id] = @cart.id
+    end
+
+    def merge_guest_cart_if_exists
+      return unless session[:cart_id]
+      
+      guest_cart = Cart.find_by(id: session[:cart_id])
+      if guest_cart && guest_cart.id != @cart.id
+        guest_cart.line_items.each do |item|
+          existing_item = @cart.line_items.find_by(product_id: item.product_id)
+          
+          if existing_item
+            existing_item.update(quantity: existing_item.quantity + item.quantity)
+          else
+            new_item = @cart.line_items.build(
+              product_id: item.product_id,
+              quantity: item.quantity
+            )
+            new_item.save
+          end
+        end
+        
+        guest_cart.destroy
+        session[:cart_id] = @cart.id
+      end
     end
 end
