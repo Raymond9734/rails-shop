9bcaa651056e860259139e85b0712bd6c4cc97ac
Raymond9734 <rymadara97@gmail.com>
2025-03-28 22:24:43 +0300

    fix(development): Correct method name for file existence check in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -83,6 +83,6 @@ Rails.application.configure do
     env_file = File.join(Rails.root, 'config', 'local_env.yml')
     YAML.load(File.open(env_file)).each do |key, value|
       ENV[key.to_s] = value
-    end if File.exists?(env_file)
+    end if File.exist?(env_file)
   end
 end
