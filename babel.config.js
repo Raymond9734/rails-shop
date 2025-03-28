5173187d595870dcba4b403e173a354898d1a49d
Raymond9734 <rymadara97@gmail.com>
2025-03-28 22:41:47 +0300

    feat(babel): Add plugins for transforming private methods and properties
    - Included '@babel/plugin-transform-private-methods' with loose option enabled.
    - Added '@babel/plugin-transform-private-property-in-object' with loose option enabled.

--- babel.config.js
+++ babel.config.js
@@ -64,6 +64,18 @@ module.exports = function(api) {
         {
           async: false
         }
+      ],
+      [
+        '@babel/plugin-transform-private-methods',
+        {
+          loose: true
+        }
+      ],
+      [
+        '@babel/plugin-transform-private-property-in-object',
+        {
+          loose: true
+        }
       ]
     ].filter(Boolean)
   }
