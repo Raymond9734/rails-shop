ea7b75289ed1893e17f85eb5fefd73b77f4f85a7
Raymond9734 <rymadara97@gmail.com>
2025-03-29 21:26:31 +0300

    chore(schema): Update database schema version and modify price column precision
    - Incremented the schema version to 2025_03_29_171006.
    - Changed the price column in the products table to have a decimal type with a precision of 10 and scale of 2, maintaining the default value of 0.0.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_01_17_164702) do
+ActiveRecord::Schema.define(version: 2025_03_29_171006) do
 
   create_table "access_tokens", force: :cascade do |t|
     t.string "token"
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2025_01_17_164702) do
     t.string "condition"
     t.string "finish"
     t.string "title"
-    t.decimal "price", precision: 5, scale: 2, default: "0.0"
+    t.decimal "price", precision: 10, scale: 2, default: "0.0"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
