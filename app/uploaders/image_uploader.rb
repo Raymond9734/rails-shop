64c4358276b415b59ea37deeb212ae323736ea36
Raymond9734 <rymadara97@gmail.com>
2025-03-29 22:07:23 +0300

    feat(image_uploader): Integrate Cloudinary for image storage and adjust storage settings
    - Included Cloudinary support for image uploads.
    - Configured storage to use file storage in non-production environments.
    - Added versioning for uploaded images with a thumbnail version.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,8 +1,16 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
+  include Cloudinary::CarrierWave
 
-  storage :file
+  # Choose what kind of storage to use for this uploader
+  # Use file storage for development and test, cloudinary for production
+  if Rails.env.production?
+    # No need to specify storage as Cloudinary handles it
+  else
+    storage :file unless Rails.env.production? || Rails.env.development?
+  end
 
+  # Keep this for local development
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
@@ -12,6 +20,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     ActionController::Base.helpers.asset_path("fallback/default.png")
   end
 
+  # Create different versions of your uploaded files
   version :thumb do
     process resize_to_fit: [400, 300]
   end
